e44ea8d962a3b57d43bbfe05c21208c6fee2076b
takosuke888 <cyberaura_0211@yahoo.co.jp>
2025-03-29 15:39:27 +0900

    GPTが入力モーダルデザインを提案

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,15 +32,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Todo> _todoList = [];
-  final TextEditingController _textController = TextEditingController();
 
-  void _addTodo(String text) {
-    if (text.isNotEmpty) {
-      setState(() {
-        _todoList.add(Todo(title: text));
-        _textController.clear(); // すべてのTextEditingControllerが対象？
-      });
-    }
+  void _addTodo(Todo todo) {
+    setState(() {
+      _todoList.add(todo);
+    });
   }
 
   void _removeTodo(int index) {
@@ -50,8 +47,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _toggleDone(int index) {
     setState(() {
-      _todoList[index].isDone = !_todoList[index].isDone; // Toggle the isDone property
+      _todoList[index].isDone = !_todoList[index].isDone;
     });
+  }
+
+  void _showAddTodoModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return AddTodoModal(onAdd: _addTodo);
+      },
+    );
   }
 
   @override
@@ -60,30 +67,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _textController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter TODO',
-              ),
-              onSubmitted: _addTodo,
-            ),
-          ),
-          Expanded(
-            child: TodoList(
-              todos: _todoList,
-              onToggle: _toggleDone,
-              onRemove: _removeTodo,
-            ),
-          ),
-        ],
+      body: Expanded(
+        child: TodoList(
+          todos: _todoList,
+          onToggle: _toggleDone,
+          onRemove: _removeTodo,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _addTodo(_textController.text),
+        onPressed: () => _showAddTodoModal(context),
         child: const Icon(Icons.add),
       ),
     );
@@ -140,6 +132,15 @@ class TodoCard extends StatelessWidget {
             decoration: todo.isDone ? TextDecoration.lineThrough : null,
           ),
         ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (todo.link.isNotEmpty)
+              Text("Link: ${todo.link}", style: TextStyle(color: Colors.blue)),
+            if (todo.deadline != null)
+              Text("Deadline: ${DateFormat('yyyy-MM-dd').format(todo.deadline!)}"),
+          ],
+        ),
         leading: Checkbox(
           value: todo.isDone,
           onChanged: (_) => onToggle(),
@@ -153,9 +154,79 @@ class TodoCard extends StatelessWidget {
   }
 }
 
+class AddTodoModal extends StatefulWidget {
+  final Function(Todo) onAdd;
+
+  const AddTodoModal({super.key, required this.onAdd});
+
+  @override
+  _AddTodoModalState createState() => _AddTodoModalState();
+}
+
+class _AddTodoModalState extends State<AddTodoModal> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _linkController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _submit() {
+    if (_titleController.text.isNotEmpty) {
+      widget.onAdd(Todo(
+        title: _titleController.text,
+        link: _linkController.text,
+        deadline: _selectedDate,
+      ));
+      Navigator.pop(context);
+    }
+  }
+
+  void _pickDate() async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _titleController,
+            decoration: const InputDecoration(labelText: 'TODOタイトル'),
+          ),
+          TextField(
+            controller: _linkController,
+            decoration: const InputDecoration(labelText: 'リンク (任意)'),
+          ),
+          TextButton(
+            onPressed: _pickDate,
+            child: Text(_selectedDate == null ? '期限を選択' : DateFormat('yyyy-MM-dd').format(_selectedDate!)),
+          ),
+          ElevatedButton(
+            onPressed: _submit,
+            child: const Text('追加'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class Todo {
   String title;
+  String link;
+  DateTime? deadline;
   bool isDone;
 
-  Todo({required this.title, this.isDone = false});
+  Todo({required this.title, this.link = '', this.deadline, this.isDone = false});
 }
